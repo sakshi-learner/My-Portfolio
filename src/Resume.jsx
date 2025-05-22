c966b7635b18f30edb95870096b71ad2e787bd94
sakshi-learner <144124963+sakshi-learner@users.noreply.github.com>
2025-05-22 16:50:12 +0530

    commit

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -1,10 +1,18 @@
 import "./Resume.css";
+import Link from "@material-ui/core/Link";
 function Resume(){
 
     return(
         <>
          <div className="resume">
-          <img src="https://media.licdn.com/dms/image/v2/D5622AQHItB_bFBh4zA/feedshare-shrink_800/B56ZTGGF3QGoAg-/0/1738490274118?e=1741219200&v=beta&t=t1F5g2Pc8-7WChcfXR9hZgsjPtvcup9Nt08znMW8z68" alt="resume"/>
+          <Link 
+                href="https://docs.google.com/document/d/1Vxu-ZKUMLpSqyyvuptxsXsd3PpCYjVe3/edit?usp=sharing&ouid=111824533363690068765&rtpof=true&sd=true"
+                onClick={() => {
+                    alert("Redirecting Resume");
+                }}
+            >
+             <span className="link">Click : MY RESUME</span>
+            </Link>
          </div>
         </>
     )
